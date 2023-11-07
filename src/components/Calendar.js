7dbb30894039cc7163b37eebd449eb777254ae4c
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-11-08 00:02:22 +0100

    u

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -264,7 +264,7 @@ export default class IFMCalendar extends HTMLElement {
           viewContent: jQuery(_shadowRoot.getElementById(_id + "_oView")).html(),
         });
         oView.placeAt(content);
-        oCalendar = oView.byId("calendar");
+        var oCalendar = oView.byId("calendar");
         oCalendar.setVisible(true);
       };
 
